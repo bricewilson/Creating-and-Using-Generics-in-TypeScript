e1ec8c00aeaf5a3caa3852e61807816f7e6922c2
Brice Wilson <brice@5wilsons.net>
2019-10-17 08:49:48 -0400

    recorded demo 1

---
+++ main.ts
@@ -0,0 +1,7 @@
+let junkDrawer: any[] = ['cool string', 42, true];
+
+let companies: Array<string> = ['Microsoft', 'Google', 'Amazon'];
+
+let primeNums: Array<number> = [7, 11, 13];
+
+let lastValue = primeNums.pop();
