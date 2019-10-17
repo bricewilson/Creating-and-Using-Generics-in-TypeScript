51758d58c6c95081f920be11dc1012052444b17a
Brice Wilson <brice@5wilsons.net>
2019-10-17 09:34:26 -0400

    removed code

--- main.ts
+++
@@ -1,7 +0,0 @@
-let junkDrawer: any[] = ['cool string', 42, true];
-
-let companies: Array<string> = ['Microsoft', 'Google', 'Amazon'];
-
-let primeNums: Array<number> = [7, 11, 13];
-
-let lastValue = primeNums.pop();
