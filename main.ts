9cb18272a69975ab59cb27070f597d50aebec6b2
Brice Wilson <brice@5wilsons.net>
2019-10-24 07:47:39 -0400

    recorded demo 1

--- main.ts
+++ main.ts
@@ -1,7 +1,32 @@
-let junkDrawer: any[] = ['cool string', 42, true];
+import { MeetingResource } from './models/meetingResource';
+import { ConferenceRoom, conferenceRoomData } from './models/conferenceRoom';
+import { PartyTent, partyTentData } from './models/partyTent';
+import { Building, buildingData } from './models/building';
 
-let companies: Array<string> = ['Microsoft', 'Google', 'Amazon'];
+function getBigRooms<T extends MeetingResource>(rooms: Array<T>, minSize: number): Array<T> {
 
-let primeNums: Array<number> = [7, 11, 13];
+  let bigRooms: Array<T> = [];
 
-let lastValue = primeNums.pop();
+  rooms.forEach(r => {
+    if (r.capacity > minSize) {
+      bigRooms.push(r);
+    }
+  });
+
+  return bigRooms;
+
+}
+
+let bigRooms = getBigRooms(buildingData, 20);
+
+console.log(bigRooms);
+
+// function shortenArray<T>(data: Array<T>, amountToShorten: number): Array<T> {
+//   return data.splice(amountToShorten, data.length);
+// }
+
+// let stringArray: string[] = ['Visual Basic', 'C++', 'TypeScript', 'JavaScript'];
+
+// let fewerLanguages = shortenArray<string>(stringArray, 2);
+
+// console.log(fewerLanguages);
