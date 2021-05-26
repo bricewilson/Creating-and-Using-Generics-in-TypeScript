73b73dea88588211d337123a5a355d893bf79e42
Brice Wilson <brice@5wilsons.net>
2021-05-26 15:50:51 -0400

    recorded demo 3

--- main.ts
+++ main.ts
@@ -17,15 +17,22 @@ function getBigRooms<T extends MeetingResource>(rooms: Array<T>, minSize: number
 
 }
 
-let bigRooms = getBigRooms(buildingData, 20);
+let getLargeRooms: <T extends MeetingResource>(rooms: Array<T>, minSize: number) => Array<T>;
 
-console.log(bigRooms);
+getLargeRooms = getBigRooms;
+
+let largeRooms = getLargeRooms(conferenceRoomData, 30);
+
+console.log(largeRooms);
 
 
+function shortenArray<T>(data: Array<T>, amountToShorten: number): Array<T> {
+  return data.splice(amountToShorten, data.length);
+}
 
-// function shortenArray<T>(data: Array<T>, amountToShorten: number): Array<T> {
-//   return data.splice(amountToShorten, data.length);
-// }
+let shrinkArray: <U>(original: Array<U>, units: number) => Array<U>;
+
+shrinkArray = shortenArray;
 
 // let stringArray: string[] = ['Visual Basic', 'C++', 'TypeScript', 'JavaScript'];
 
