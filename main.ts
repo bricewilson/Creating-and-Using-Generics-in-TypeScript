b2e35e5dfb656fca54ed28a883b813ab82e37f62
Brice Wilson <brice@5wilsons.net>
2021-05-26 15:12:06 -0400

    recorded demo 2

--- main.ts
+++ main.ts
@@ -1,9 +1,34 @@
-function shortenArray<T>(data: Array<T>, amountToShorten: number): Array<T> {
-  return data.splice(amountToShorten, data.length);
+import { MeetingResource } from './models/meetingResource';
+import { ConferenceRoom, conferenceRoomData } from './models/conferenceRoom';
+import { PartyTent, partyTentData } from './models/partyTent';
+import { Building, buildingData } from './models/building';
+
+function getBigRooms<T extends MeetingResource>(rooms: Array<T>, minSize: number): Array<T> {
+
+  let bigRooms: Array<T> = [];
+
+  rooms.forEach(r => {
+    if (r.capacity > minSize) {
+      bigRooms.push(r);
+    }
+  });
+
+  return bigRooms;
+
 }
 
-let stringArray: string[] = ['Visual Basic', 'C++', 'TypeScript', 'JavaScript'];
+let bigRooms = getBigRooms(buildingData, 20);
 
-let fewerLanguages = shortenArray<string>(stringArray, 2);
+console.log(bigRooms);
 
-console.log(fewerLanguages);
+
+
+// function shortenArray<T>(data: Array<T>, amountToShorten: number): Array<T> {
+//   return data.splice(amountToShorten, data.length);
+// }
+
+// let stringArray: string[] = ['Visual Basic', 'C++', 'TypeScript', 'JavaScript'];
+
+// let fewerLanguages = shortenArray<string>(stringArray, 2);
+
+// console.log(fewerLanguages);
